d7508a59dc36842189bf5e44c7bbc4c730be8438
IrynaZolkina <wzyz7557@hotmail.com>
2020-04-10 17:18:09 -0700

    Updates

---
+++ precache-manifest.4595eab36029a27ad23984f982e0af47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e0a5fd17dc9191b700a0148b01d743",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7d6c3a06fc81e430867a",
+    "url": "/monsters-rolodex/static/css/main.0de4cc12.chunk.css"
+  },
+  {
+    "revision": "43da135bfdefe781bb60",
+    "url": "/monsters-rolodex/static/js/2.779dc4c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.779dc4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6c3a06fc81e430867a",
+    "url": "/monsters-rolodex/static/js/main.b9a56144.chunk.js"
+  },
+  {
+    "revision": "34f87f932ebc75d95f8d",
+    "url": "/monsters-rolodex/static/js/runtime-main.4d40c4c2.js"
+  }
+]);
